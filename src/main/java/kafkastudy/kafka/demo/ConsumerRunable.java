cf3b96dd05b8360ab9b765b53acc305fd4eac973
宏昌 <1161774437@qq.com>
2017-06-07 10:49:00 +0800

    项目初始化

---
+++ src/main/java/kafkastudy/kafka/demo/ConsumerRunable.java
@@ -0,0 +1,82 @@
+package kafkastudy.kafka.demo;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class ConsumerRunable implements Runnable{
+	
+	 /**
+	  * topic
+	  */
+	 private final static String topic = "test4";
+	 
+	 private JdbcTemplate jdbcTemplate;
+	 /**
+	 * ip地址和端口
+	 */
+	 private final static String ipAndPort = "192.168.95.73:9092";
+	 
+	 private KafkaConsumer<String, String> consumer;
+	
+	
+	 public ConsumerRunable() {
+		//DataSource ds = new DriverManagerDataSource("jdbc:mysql://192.168.95.70:3306/chigoo", "root", "beyondsoft");
+		 
+		 // 创建数据源（连接池）dbcp
+		
+	    BasicDataSource basicDataSource = new BasicDataSource();
+	
+	    // 基本4项
+	    basicDataSource.setDriverClassName("com.mysql.jdbc.Driver"); // 加载驱动
+	    basicDataSource.setUrl("jdbc:mysql://192.168.95.70:3306/chigoo");  // 数据库的
+	    basicDataSource.setUsername("root");
+	    basicDataSource.setPassword("beyondsoft");
+		jdbcTemplate = new JdbcTemplate(basicDataSource);
+        Properties props = new Properties();
+        props.put("bootstrap.servers", ipAndPort);
+        props.put("group.id", "abc");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+	}
+	
+	@Override
+	public void run() {
+		while(true){
+			ConsumerRecords<String, String> records = consumer.poll(1000);
+            //利用
+            for (ConsumerRecord<String, String> record : records){
+            	
+            	String sql = "insert into sample(s_code,s_desc) values ('"+record.key()+"','"+record.value()+"')";
+            	jdbcTemplate.execute(sql);
+            	//log.info("offset = %d, key = "+ record.offset()+", value = "+record.value()+"\n" );
+            	System.out.printf("offset = %d, key = %s, value = %s ,partition=%s,thread = %s\n,", record.offset(), record.key(), record.value(),record.partition(),Thread.currentThread());
+            }
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<50){
+			Thread t = new Thread(new ConsumerRunable());
+			t.start();
+			i++;
+		}
+//		Thread t = new Thread(new ConsumerRunable());
+//		t.start();
+	}
+}
---
+++ src/main/java/kafkastudy/kafka/demo/KafkaProducerCase.java
@@ -0,0 +1,76 @@
+package kafkastudy.kafka.demo;
+
+
+import java.util.Properties;
+import java.util.UUID;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+
+public class KafkaProducerCase {
+	/**
+	 * 生成者
+	 */
+	private static Producer<String, String> producer;
+	
+	/**
+	 * topic
+	 */
+	private final static String topic = "test4";
+	
+	/**
+	 * 并发数量
+	 */
+	private final static int num = 50000;
+	
+	/**
+	 * ip地址和端口
+	 */
+	private final static String ipAndPort = "192.168.95.73:9092";
+	
+	public static void main(String[] args) {
+		 Properties props = new Properties();
+		 props.put("bootstrap.servers", ipAndPort);
+         props.put("acks", "all");
+         props.put("retries", 0);
+         props.put("batch.size", 16384);
+         props.put("buffer.memory", 33554432);
+         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 
+         producer = new KafkaProducer<>(props);
+         
+         KafkaProducerCase a = new KafkaProducerCase();
+		 int i=0;
+		 while(i<num){
+			 a.new ProducerThread(i).run();
+			 i++;
+		 }
+		 producer.close();
+		 
+	}
+	
+	/**
+	 * 生成者线程
+	 * @author daihongchang
+	 *
+	 */
+	class ProducerThread implements Runnable{
+		
+		private int i;
+		
+		public ProducerThread(int arg){
+			i = arg;
+		}
+		
+		@Override
+		public void run() {
+			 String arg = UUID.randomUUID().toString();
+			 producer.send(new ProducerRecord(topic, arg, "ccddee"));
+		}
+		
+	}
+}
